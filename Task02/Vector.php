f4b09eedc216a8aa43b478dd0b226439c438dc03
shuryginaKN <ks5614741@gmail.com>
2024-02-24 14:48:56 +0300

    Task02

--- Task02/Vector.php
+++ Task02/Vector.php
@@ -1,78 +1,67 @@
 <?php
 
-class Vector 
+namespace Vector;
+
+class Vector
 {
     private $x;
     private $y;
     private $z;
-    
+
     public function __construct($x, $y, $z)
     {
-        if (!is_numeric($x) || !is_numeric($y) || !is_numeric($z)) {
-            throw new InvalidArgumentException('Coordinates must be numeric');
+        if (is_numeric($x) && is_numeric($y) && is_numeric($z)) {
+            $this->x = $x;
+            $this->y = $y;
+            $this->z = $z;
+        } else {
+            throw new InvalidArgumentException('Координаты вектора должны быть числами.');
         }
-        
-        $this->x = $x;
-        $this->y = $y;
-        $this->z = $z;
     }
 
     public function add(Vector $vector): Vector
     {
         return new Vector(
-            $this->x + $vector->x, 
+            $this->x + $vector->x,
             $this->y + $vector->y,
             $this->z + $vector->z
-            );
+        );
     }
 
     public function sub(Vector $vector): Vector
     {
         return new Vector(
-            $this->x - $vector->x, 
+            $this->x - $vector->x,
             $this->y - $vector->y,
             $this->z - $vector->z
-            );
+        );
     }
 
     public function product($number): Vector
     {
         return new Vector(
-            $this->x * $number, 
+            $this->x * $number,
             $this->y * $number,
             $this->z * $number
-            );
+        );
     }
 
     public function scalarProduct(Vector $vector): float
     {
         return $this->x * $vector->x + $this->y * $vector->y + $this->z * $vector->z;
-
     }
 
     public function vectorProduct(Vector $vector): Vector
     {
-        $newX = $this->y * $vector->z - $this->z * $vector->y;
-        $newY = $this->z * $vector->x - $this->x * $vector->z;
-        $newZ = $this->x * $vector->y - $this->y * $vector->x;
-
-        return new Vector($newX, $newY, $newZ);
+        return new Vector(
+            $this->y * $vector->z - $this->z * $vector->y,
+            $this->z * $vector->x - $this->x * $vector->z,
+            $this->x * $vector->y - $this->y * $vector->x
+        );
     }
 
     public function __toString()
     {
         return "($this->x; $this->y; $this->z)";
     }
-
 }
-
-// $vector1 = new Vector(1, 2, 3);
-// $vector2 = new Vector(-1, 0, 2);
-
-// echo $vector1->add($vector2) . "<br>";
-// echo $vector1->sub($vector2) . "\n";
-// echo $vector1->product(2) . "\n";
-// echo $vector1->scalarProduct($vector2) . "\n";
-// echo $vector1->vectorProduct($vector2) . "\n";
-
-?>
--- Task02/run_test.php
+++ Task02/run_test.php
@@ -1,7 +1,6 @@
 <?php
 
     require_once 'Test.php';
+    require_once 'Vector.php';
 
-    runTest();
-
-?>
+runTest();
