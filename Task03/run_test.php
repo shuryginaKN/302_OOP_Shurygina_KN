5a5bf167676e85d7a3f92ff17cd265b7c91ef124
shuryginaKN <ks5614741@gmail.com>
2024-03-16 22:03:58 +0300

    2 commit

---
+++ Task03/run_test.php
@@ -0,0 +1,7 @@
+<?php
+
+    require_once 'Book.php';
+    require_once 'BooksList.php';
+    require_once 'Test.php';
+
+runTest();
---
+++ Task03/src/BooksList.php
@@ -0,0 +1,53 @@
+<?php
+
+// namespace Model;
+
+// use Model\Book;
+require_once 'Book.php';
+
+class BooksList
+{
+    private $books = array();
+
+    public function add(Book $book)
+    {
+        $this->books[] = $book;
+    }
+
+    public function count() : int 
+    {
+        return count($this->books);
+    }
+
+    public function get(int $n) : Book
+    {
+        if ($n > 0 && $n <= count($this->books))
+        {
+            return $this->books[$n-1];
+        }
+    }
+
+    public function store(string $fileName)
+    {
+        $result = file_put_contents($fileName, serialize($this->books));
+
+        if ($result === false) {
+            echo "Ошибка при записи в файл";
+        } else {
+            echo "Данные успешно записаны в файл";
+        }
+    }
+
+    public function load(string $fileName)
+    {
+        if (file_exists($fileName)) {
+            $this->books = unserialize(file_get_contents($fileName));
+        } else {
+            echo 'Файл не найден';
+        }
+    }
+
+    
+}
+
+?>
